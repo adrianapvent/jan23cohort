5884cf54412063f7804d6a13c323af5f990afd0c
adrianapvent <adriana.e.pena@gmail.com>
2023-03-20 22:05:18 -0500

    FirstJavaProgram

---
+++ assignments/Java/week1/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package assignments.Java.week1;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Adriana Pena");
+        System.out.println("I am no longer in my 20's");
+        System.out.println("My hometown is Zapata, Tx.");
+    }
+}
