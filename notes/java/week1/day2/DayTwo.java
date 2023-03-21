4c4514b4056ef84c0d45df1718853294f8629fa3
adrianapvent <adriana.e.pena@gmail.com>
2023-03-21 12:03:44 -0500

    java notes

---
+++ notes/java/week1/day2/DayTwo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DayTwo {
+    public static void main(String[] args) {
+        String[] words = {"Hi", "how", "are", "you"};
+        ArrayList<String> phrase = new ArrayList<String>(0);
+
+        phrase.add("hi");
+        phrase.add("hello");
+        phrase.add("how");
+        phrase.add("are");
+        phrase.add("you");
+
+        System.out.println(phrase);
+    }
+}
