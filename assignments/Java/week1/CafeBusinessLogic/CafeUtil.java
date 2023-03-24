260b7a5b68edb742b9f0dc803eb1045fb2734f72
adrianapvent <adriana.e.pena@gmail.com>
2023-03-24 16:30:32 -0500

    CafebusinessLogic functioning

--- assignments/Java/week1/CafeBusinessLogic/CafeUtil.java
+++ assignments/Java/week1/CafeBusinessLogic/CafeUtil.java
@@ -1,4 +1,32 @@
-
+import java.util.ArrayList;
 public class CafeUtil {
-    
+    public static int getStreakGoal() {
+        int sum = 0;
+        for(int i = 0; i <= 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static double getOrderTotal(double[] prices) {
+        double sum = 0;
+        for (double price :prices) {
+            sum += price;
+        }
+        return sum;
+    }
+
+    public void displayMenu(ArrayList<String> menuItems) {
+        System.out.println(menuItems);
+
+    }
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello," + userName + "!");
+        System.out.println("There are" + " " + customers.size() + " " + "people in front of you.");
+        customers.add(userName);
+        System.out.println(customers);
+    }
 }
--- assignments/Java/week1/CafeBusinessLogic/TestCafe.java
+++ assignments/Java/week1/CafeBusinessLogic/TestCafe.java
@@ -3,6 +3,25 @@ import java.util.Arrays;
 
 public class TestCafe {
     public static void main(String[] args) {
-        
+
+        CafeUtil infoCafeUtil = new CafeUtil();
+
+        System.out.printf(" \nStreak Goal = %s \n", infoCafeUtil.getStreakGoal());
+                
+        double[] totalItems = {1, 2, 3, 4, 5};
+        System.out.printf("Double Items Total = %s \n", infoCafeUtil.getOrderTotal(totalItems));
+
+        ArrayList<String> menuItems = new ArrayList<String>();
+        menuItems.add("drip coffee");
+        menuItems.add("cappucino");
+        menuItems.add("latte");
+        menuItems.add("mocha");
+        infoCafeUtil.displayMenu(menuItems);
+
+        ArrayList<String> customers = new ArrayList<String>();
+        for(int i = 0; i < 5; i++) {
+            infoCafeUtil.addCustomer(customers);
+            System.out.println("\n");
+        }
     }
 }
