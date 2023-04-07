60ce3b7424840809d1bf46af44230dc56c8708d9
adrianapvent <adriana.e.pena@gmail.com>
2023-04-07 18:25:45 -0500

    Zookeeper completed

--- assignments/Java/week1/Zookeeper/src/main/java/com/codingdojo/zookeeper/ZookeeperApplication.java
+++ assignments/Java/week1/Zookeeper/src/main/java/com/codingdojo/zookeeper/ZookeeperApplication.java
@@ -15,11 +15,29 @@ public class ZookeeperApplication {
 		//gorilla test
 		Gorilla gorilla = new Gorilla();
 		gorilla.throwSomething();
+		gorilla.throwSomething();
+		gorilla.throwSomething();
 		
+		gorilla.eatBananas();
+		gorilla.eatBananas();
+		
+		gorilla.climb();
+		gorilla.displayEnergy();
 		
 		// bat tests
 		Bat bat = new Bat();
 		bat.attackTown();
+		bat.attackTown();
+		bat.attackTown();
+		
+		bat.eatHumans();
+		bat.eatHumans();
+		
+		bat.fly();
+		bat.fly();
+		bat.displayEnergy();
+		
+		
 	}
 	
 
